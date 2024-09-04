6d3a60f60f3143c33c89f503973741da7d78cc9a
Fabio <fabio.cantarani@gmail.com>
2024-09-04 13:22:21 -0300

    Bug Fixes

--- OracaoApp-API/Program.cs
+++ OracaoApp-API/Program.cs
@@ -2,10 +2,9 @@ using OracaoApp.API.Extensions;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
 builder.AddCustomServices();
+
 var app = builder.Build();
 app.UseCustomServices();
 
