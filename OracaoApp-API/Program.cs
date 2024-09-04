6c65cb62b8d0d4c15720689ab581f9d762b4e299
Fabio <fabio.cantarani@gmail.com>
2024-09-04 13:13:49 -0300

    Enhancements

--- OracaoApp-API/Program.cs
+++ OracaoApp-API/Program.cs
@@ -6,9 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 builder.AddCustomServices();
-
 var app = builder.Build();
-
 app.UseCustomServices();
 
 app.Run();
